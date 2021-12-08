b2bd3639eac0b5faa31dc18f1de85ec7b4e7d373
robotxschool <72185662+robotxschool@users.noreply.github.com>
2021-12-08 14:35:22 +0300

    Update solution.py

--- packages/circle_drive/scripts/solution.py
+++ packages/circle_drive/scripts/solution.py
@@ -173,10 +173,10 @@ def solution(obs):
         steering = steering / len(average_pose)
     
     if -0.12 < steering < 0.12 and steering != 0:
-        vel = 0.28  # ускоряемся при движении прямо
+        vel = 0.22  # ускоряемся при движении прямо
         steering = steering * 0.95  # и корректируем руль на большой скорости
     else:
-        vel = 0.28  # скорость во время поворота
+        vel = 0.22  # скорость во время поворота
     if steering>1:
         steering=1
     if steering<-1:
