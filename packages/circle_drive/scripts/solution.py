dc1a3f65871ba55234968024b8cdac6b39d10c0d
robotxschool <72185662+robotxschool@users.noreply.github.com>
2021-12-08 14:23:39 +0300

    Update solution.py

--- packages/circle_drive/scripts/solution.py
+++ packages/circle_drive/scripts/solution.py
@@ -156,7 +156,7 @@ def solution(obs):
         y_deviation = -80
 
     pose = -(w_deviation + y_deviation) / 1000
-    kP = 3  # основной коэффициент усиления поворота колес
+    kP = 4  # основной коэффициент усиления поворота колес
     if y_line_pose_y > 350 and y_angles < 30:
         kP = 4  # коэффициент, если обнаружена желтая разметка почти горизонтально близко к роботу
     steering = kP * pose
@@ -176,7 +176,7 @@ def solution(obs):
         vel = 0.25  # ускоряемся при движении прямо
         steering = steering * 0.95  # и корректируем руль на большой скорости
     else:
-        vel = 0.16  # скорость во время поворота
+        vel = 0.18  # скорость во время поворота
     print("steering", steering,"speed",vel)
     return [vel, steering]
 # import cv2
